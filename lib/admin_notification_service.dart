110ab663cc06f52962574f3757dce688e390ff64
Pola <your-email@example.com>
2026-06-26 14:08:20 +0100

    now

--- lib/admin_notification_service.dart
+++ lib/admin_notification_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
-import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/appwrite.dart' as appwrite;
+import 'package:appwrite/src/enums.dart' as appwrite_enums;
 
 import 'app_auth_service.dart';
 import 'appwrite_config.dart';
@@ -11,7 +12,8 @@ class AdminNotificationService {
   static final AdminNotificationService instance =
       AdminNotificationService._();
 
-  late final Functions _functions = Functions(AppAuthService.instance.client);
+  late final appwrite.Functions _functions =
+      appwrite.Functions(AppAuthService.instance.client);
 
   Future<void> sendNotification({
     required String title,
@@ -26,7 +28,7 @@ class AdminNotificationService {
 
     await _functions.createExecution(
       functionId: appwriteAdminNotificationFunctionId,
-      method: ExecutionMethod.pOST,
+      method: appwrite_enums.ExecutionMethod.pOST,
       body: jsonEncode(payload),
     );
   }
